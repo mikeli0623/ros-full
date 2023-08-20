490aef1fbca10021cb11fcf4b07a7271b68da4f8
lim211 <lim211@mcmaster.ca>
2023-08-20 01:21:50 -0400

    Cleanup deleted files

--- client/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  prefix: "tw-",
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
--- server/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { register, login, logout } = require("../controllers/auth");
-const router = express.Router();
-
-router.post("/register", register);
-router.post("/login", login);
-router.get("/logout", logout);
-
-module.exports = router;
